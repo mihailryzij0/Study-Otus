c2c1dcd52d90dcd06c6b790ec86f323e7d8dac2b
mihailryzij0 <mihailryzij0@gmail.com>
2021-11-08 14:00:46 +0300

    changed module-3 / task-1

--- src/module-3/task-1/multiTable.js
+++ src/module-3/task-1/multiTable.js
@@ -3,7 +3,7 @@ export function multiTable(number) {
   if (number > 10 || number < 1 || typeof number !== "number") {
     return console.log(NaN);
   }
-  for (let i = 1; i <= 10; i + 1) {
+  for (let i = 1; i <= 10; i += 1) {
     result.push(`${i} * ${number} = ${i * number}`);
   }
   return console.log(result);
